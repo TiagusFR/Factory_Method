ec8a559039b12ae015f24386e3c5d950b562a63c
Tiago <Tiago@TIAGO>
2024-08-12 16:30:52 -0300

    Add project files.

---
+++ Factory_Method/Point.cs
@@ -0,0 +1,27 @@
+﻿namespace Factory_Method
+{
+    public class Point
+    {
+        private double x;
+        private double y;
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoinrt(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta),rho * Math.Sin(theta));
+        }
+
+        public double X => x;
+        public double Y => y;
+    }
+}
---
+++ Factory_Method/Program.cs
@@ -0,0 +1,15 @@
+﻿using Factory_Method;
+
+public class Program
+{
+    public static void Main()
+    {
+        Point cartesianPoint = Point.NewCartesianPoint(3, 4);
+        Console.WriteLine($"Cartesian Point: X = {cartesianPoint.X}, Y = {cartesianPoint.Y}");
+
+        double rho = 5;
+        double theta = Math.PI / 4;
+        Point polarPoint = Point.NewPolarPoinrt(rho, theta);
+        Console.WriteLine($"Polar Point: X = {polarPoint.X}, Y = {polarPoint.Y}");
+    }
+}
